151bb447f553cf24b15f4af7437ec45ec6e8db8f
ifeanyi divine onuoha <ifeanyidivineonuoha@gmail.com>
2022-11-26 09:26:38 +0100

    used classes instead of functions

--- script/script.js
+++ script/script.js
@@ -1,44 +1,54 @@
-'use strict';
-const form = document.getElementById('login');
+class ValidateForm {
+  constructor(form) {
+    this.form = form;
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onFocus = this.onFocus.bind(this);
 
-form.addEventListener('submit', (event) => {
-  let result = validateForm(form.elements);
-
-  if (!result) {
-    event.preventDefault();
+    form.addEventListener('submit', this.onSubmit);
+    form.addEventListener('focus', this.onFocus, true);
   }
-});
 
-form.addEventListener('focus', (event) => {
-  let target = event.target;
+  onSubmit(event) {
+    let result = this.validateInputs(this.form.elements);
 
-  if (target.tagName == 'INPUT') {
-    let div = target.closest('div');
-    if (div.classList.contains('invalid')) {
-      div.classList.remove('invalid');
+    if (!result) {
+      event.preventDefault();
     }
   }
-}, true);
 
+  onFocus(event) {
+    let target = event.target;
 
-function validateForm(elements) {
-  let emailValidator = /^[a-z]+@[a-z]+(\.[a-z]+){1,2}$/;
-  let isValid = true;
-
-  for (let elem of elements) {
-    if (elem.value == '' && elem.tagName !== 'BUTTON') {
-      elem.closest('div').classList.add('invalid');
-      isValid = false;
-    }
-
-    if (elem.name == 'email') {
-      let value = elem.value;
-      isValid = emailValidator.test(value);
-      if (!isValid) {
-        elem.closest('div').classList.add('invalid');
+    if (target.tagName == 'INPUT') {
+      let div = target.closest('div');
+      if (div.classList.contains('invalid')) {
+        div.classList.remove('invalid');
       }
     }
   }
 
-  return isValid;
+  validateInputs(elements) {
+    const email = /^[a-z\d-\.]+@[a-z\d-]+(\.[a-z]+){1,2}$/;
+    let isValid = true;
+
+    for (let input of elements) {
+      if (input.value == '' && input.tagName !== 'BUTTON') {
+        input.closest('div').classList.add('invalid');
+        isValid = false;
+      }
+
+      if (input.name == 'email') {
+        let value = input.value;
+        isValid = email.test(value);
+        if (!isValid) {
+          input.closest('div').classList.add('invalid');
+        }
+      }
+    }
+
+    return isValid;
+  }
 }
+
+const form = document.getElementById('login');
+new ValidateForm(form);
